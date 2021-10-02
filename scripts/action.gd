f24098971e30c72756d07bb40d062cb3a1f2d331
Alberto Blanco Celdrán <flaberto.abc@gmail.com>
2021-10-02 16:12:18 +0200

    Added basic movement for the cat

---
+++ scripts/action.gd
@@ -0,0 +1,16 @@
+
+
+# Action
+# @author Alberto Blanco Celdrán
+# @date 02/10/2021 15:40
+# Base class for cat actions in the hub
+# @deprecated
+
+class_name Action
+
+var cat: Node2D
+var tick: Timer
+
+# constructor
+func _init(cat: Node2D):
+	self.tick = tick
