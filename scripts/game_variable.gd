a9e2f98c7587af37a997eef36b257334600a67fd
Alberto Blanco Celdrán <flaberto.abc@gmail.com>
2021-10-02 13:41:27 +0200

    Added Game State, ticking and cat health (#34)
    * Added Game State, ticking and health
    
    * Added android/build to the gitignore
    
    * Removed android build files

---
+++ scripts/game_variable.gd
@@ -0,0 +1,22 @@
+extends Node
+
+
+# Game state variable
+# @author Alberto Blanco Celdrán
+# @date 02/10/2021 10:27
+# The base for all Game State variables.
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+# Resets the variable to it's default state.
+func reset():
+	pass
