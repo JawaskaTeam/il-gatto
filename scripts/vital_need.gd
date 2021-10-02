5f88782485b30cbd26a96f3e50e71cae0b400dfa
Alberto Blanco Celdrán <flaberto.abc@gmail.com>
2021-10-02 13:57:43 +0200

    Added vital needs mechanic, hunger and thirst

---
+++ scripts/vital_need.gd
@@ -0,0 +1,30 @@
+extends 'res://scripts/need.gd'
+
+
+# Vital need
+# @author Alberto Blanco Celdrán
+# @date 02/10/2021 13:48
+# A need that will constantly decrease and upon reaching 0, it will affect health
+
+export (NodePath) var health
+export (float) var expense_rate # how much is decreased per tick
+export (float) var damage_rate # how much health it decreases per tick upon reaching 0
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+# Applies expense or damage
+# @param how many ticks occured
+func tick(delta: float):
+	if get_value() > 0:
+		set_value(value - (expense_rate * delta))
+	else:
+		health.set_value(health.get_value() - (damage_rate * delta))
